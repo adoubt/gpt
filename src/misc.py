7a7867a4775c6c55b5aeb47d1f216ae69215436b
goldenkernel <124398859+adoubt@users.noreply.github.com>
2023-11-10 15:08:13 +0700

    Update misc.py

--- src/misc.py
+++ src/misc.py
@@ -2,12 +2,12 @@ from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 
 
-token="5947463331:AAFjoFVFNTzeghEzY8mB9m_RznEnwbov7ms"
-super_admin = "436839651"
+token="594746333:AAFjoFVFNTzeghEzY8mB9m_RznEnwbov7m"
+super_admin = "43683965"# not required but you can set your id
 password = '1234' # /set_admin{password}
 
 
 
-bot_id = token.split(":",1)[0]
+bot_id = token.split(":",1)[0] # temp-fix: bot still have some buds with admin rules in callback func.. 
 bot = Bot(token)
 dp = Dispatcher(storage=MemoryStorage())
